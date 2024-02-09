61c569e08ef5652a4da72aa680d69f77c930bd6d
chinthakaSenarathna <chironprabath@gmail.com>
2024-02-09 22:49:03 +0530

    Bank Account is completed...

--- src/Components/UserSystem/UserSystemComponentsSub/BankCardComponents/BankCard.js
+++ src/Components/UserSystem/UserSystemComponentsSub/BankCardComponents/BankCard.js
@@ -18,41 +18,22 @@ function BankCard(){
 
                             <div className='card d-flex flex-column justify-content-center h-100 p-3 bank-card-card w-100'>
                                 <div class="d-flex flex-row align-items-center mb-4 pb-1 bank-card-body-section-save-item">
-                                    <div class="flex-fill mx-3">                                                                           
-                                        <div class="form-outline">
-                                            <input type="text" id="formControlLgXc" class="form-control form-control-lg"
-                                                value="**** **** **** 3193" />
-                                            <label class="form-label" for="formControlLgXc">Card Number</label>
-                                        </div>                                        </div>
-                                        <a href="#!">Remove card</a>
-                                    </div>
-
-                                    <div class="d-flex flex-row align-items-center mb-4 pb-1 bank-card-body-section-save-item">
-                                        <div class="flex-fill mx-3">
-                                            <div class="form-outline">
-                                                <input type="text" id="formControlLgXs" class="form-control form-control-lg"
-                                                    value="**** **** **** 4296" />
-                                                <label class="form-label" for="formControlLgXs">Card Number</label>
-                                            </div>
-                                        </div>
-                                        <a href="#!">Remove card</a>
-                                    </div>
-                                </div>
+                                    
+                                </div>    
+                            </div>
 
                                 <p class="fw-bold mb-4 pb-2">Add new card:</p>
 
                                 <div className='card d-flex flex-column justify-content-center px-4 bank-card-card'>
                                     <div class="form-outline mb-4 bank-card-body-section-add-item">
-                                        <input type="text" id="formControlLgXsd" class="form-control form-control-lg"
-                                            value="Anna Doe" />
+                                        <input type="text" id="formControlLgXsd" class="form-control form-control-lg" />
                                         <label class="form-label" for="formControlLgXsd">Cardholder's Name</label>
                                     </div>
 
                                     <div class="row mb-4">
                                         <div class="col-7">
                                             <div class="form-outline bank-card-body-section-add-item">
-                                                <input type="text" id="formControlLgXM" class="form-control form-control-lg"
-                                                    value="1234 5678 1234 5678" />
+                                                <input type="text" id="formControlLgXM" class="form-control form-control-lg" />
                                                 <label class="form-label" for="formControlLgXM">Card Number</label>
                                             </div>
                                         </div>
